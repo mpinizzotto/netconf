a0cec3eca9a6c3a846c42f9dc5901ac5458cba3a
Matthew Pinizzotto <opusdefunk@hotmail.com>
2018-04-20 12:29:18 -0500

    update

--- nxnetconf_config_interface.py
+++ nxnetconf_config_interface.py
@@ -5,21 +5,21 @@
 
 from ncclient import manager
 
-hostname = '172.16.30.23'
+hostname = '172.16.30.24'
 username = 'admin'
 password = 'P@ssword'
 
 if __name__ == '__main__':
 
-    with manager.connect(host='172.16.30.23', 
-                         port=830, 
+    with manager.connect(host='172.16.30.24', 
+                         port=22, 
                          username='admin', 
                          password='P@ssw0rd',
                          hostkey_verify=False, 
-                         device_params={'name': 'csr'}, 
+                         device_params={'name': 'nexus'}, 
                          timeout=90) as device:
 
-        commands = ['config t', 'interface GigabitEthernet1/2', 'ip address 10.1.1.1 255.255.255.0','description CONNECTION TO RTR-02']
+        commands = ['config t', 'interface e1/1', 'description TEST']
         nc_config_reply = device.exec_command(commands)
         print nc_config_reply.xml
 		
